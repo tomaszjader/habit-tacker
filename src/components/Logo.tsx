1114356f9316cf314aecc0801ba921820b5d9c95
Tomasz Jąder <tomasz.jader@gmail.com>
2025-07-30 18:20:14 +0200

    fix Splash screen in PWA 5

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ const Logo: React.FC<LogoProps> = ({ size = 40, className = '' }) => {
       <svg 
         width="100%" 
         height="100%" 
-        viewBox="0 0 64 64" 
+        viewBox="-2 -2 68 68" 
         fill="none" 
         xmlns="http://www.w3.org/2000/svg"
         className="w-full h-full"
